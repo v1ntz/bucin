c3c08dea2ff7a6b14009e3e821d3726b6c716797
v1ntz <66353186+v1ntz@users.noreply.github.com>
2021-02-14 07:27:56 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -1,6 +1,6 @@
 export default [
-  "Teks pertama",
-  "Teks kedua",
-  "Teks ke - n",
-  "Aku sayang kamu!"
+  "This is for you",
+  "Pricilia Meisya Kurniawan",
+  "Happy V day sayang",
+  "I love you so much!"
 ];
